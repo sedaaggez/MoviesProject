ee57c418e87392268d9016a94eb64c8a89d5d6ad
Seda Aggez <sedaaggez56@gmail.com>
2020-12-29 17:33:01 +0300

    code clean

--- app/src/main/java/com/sedaaggez/moviesproject/view/MoviesFragment.kt
+++ app/src/main/java/com/sedaaggez/moviesproject/view/MoviesFragment.kt
@@ -1,8 +1,6 @@
 package com.sedaaggez.moviesproject.view
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
